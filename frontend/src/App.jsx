930a20c995a1c88713e513fbf27a16b05b224002
TwiceG <g0stron0g@gmail.com>
2024-10-27 23:07:08 +0100

    add basic setup for frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter } from 'react-router-dom';
+import AppRouter from './routes/AppRouter';
+
+
+
+function App() {
+  return (
+    <div className='App'>
+      <BrowserRouter>
+        <AppRouter />
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
