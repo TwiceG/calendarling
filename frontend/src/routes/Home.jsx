a50e98eab9ee71ea4f5c04db1a842f6caf921388
TwiceG <g0stron0g@gmail.com>
2024-10-28 01:45:23 +0100

    add nice moving welcome message css and adjust some other css

--- frontend/src/routes/Home.jsx
+++ frontend/src/routes/Home.jsx
@@ -1,13 +1,20 @@
 import 'react-calendar/dist/Calendar.css';
+import '../style/Home.css';
 
 const Home = () => {
+    const message = "WELCOME DARLING!";
 
     return (
-        <div>
-            <h1>WELCOME DARLING!</h1>
-
+        <div className="home-container">
+            <h1 className="wave-message">
+                {message.split('').map((letter, index) => (
+                    <span key={index} className="wave-letter" style={{ animationDelay: `${index * 0.1}s` }}>
+                        {letter}
+                    </span>
+                ))}
+            </h1>
         </div>
-    )
-}
+    );
+};
 
 export default Home;
