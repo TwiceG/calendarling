fc54be56aad457c68c77defe32f4e87f58b83335
TwiceG <g0stron0g@gmail.com>
2024-10-28 02:13:30 +0100

    separate welcome message on home page and added some space in between

--- frontend/src/routes/Home.jsx
+++ frontend/src/routes/Home.jsx
@@ -2,13 +2,22 @@ import 'react-calendar/dist/Calendar.css';
 import '../style/Home.css';
 
 const Home = () => {
-    const message = "WELCOME DARLING!";
+    const message1 = "WELCOME"; // First part of the message
+    const message2 = "DARLING!"; // Second part of the message
 
     return (
         <div className="home-container">
             <h1 className="wave-message">
-                {message.split('').map((letter, index) => (
+                {message1.split('').map((letter, index) => (
                     <span key={index} className="wave-letter" style={{ animationDelay: `${index * 0.1}s` }}>
+                        {letter}
+                    </span>
+                ))}
+                {/* Add space between the two messages */}
+                <span className="space">   </span>
+
+                {message2.split('').map((letter, index) => (
+                    <span key={index + message1.length} className="wave-letter" style={{ animationDelay: `${(index + message1.length) * 0.1}s` }}>
                         {letter}
                     </span>
                 ))}
