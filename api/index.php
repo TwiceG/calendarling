d9c9ae5fd1b612470eb8398eb53b37403a4c63e9
TwiceG <g0stron0g@gmail.com>
2024-11-05 23:47:57 +0100

    update the logic for index php

--- api/index.php
+++ api/index.php
@@ -1,3 +1,15 @@
 <?php
 
-require __DIR__ . '/../public/index.php';
+// Autoload dependencies
+require __DIR__ . '/../vendor/autoload.php';
+
+// Bootstrap the application
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+// Run the application
+$request = Illuminate\Http\Request::capture();
+$response = $app->handle($request);
+
+$response->send();
+
+$app->terminate($request, $response);
