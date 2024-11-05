01214a6aae843f80d6d8070a08f1f4a1cd45dd2d
TwiceG <g0stron0g@gmail.com>
2024-11-06 00:21:35 +0100

    adjust setting for vercel

--- api/index.php
+++ api/index.php
@@ -1,3 +1,17 @@
 <?php
 
-require __DIR__ . '/../public/index.php';
+// Require the Composer autoloader
+require __DIR__ . '/../backend/vendor/autoload.php';
+
+// Bootstrap the Laravel application
+$app = require_once __DIR__ . '/../backend/bootstrap/app.php';
+
+// Handle the incoming request
+$request = Illuminate\Http\Request::capture();
+$response = $app->handle($request);
+
+// Send the response back to the client
+$response->send();
+
+// Terminate the application
+$app->terminate($request, $response);
