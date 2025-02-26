5f04d2d95b18df09109ee65c539435b588242223
TwiceG <g0stron0g@gmail.com>
2025-02-26 18:48:21 +0100

    add axios cookie policy

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,15 @@ import { BrowserRouter } from 'react-router-dom';
 import AppRouter from './routes/AppRouter';
 import NavBar from './components/NavBar';
 import './style/App.css';
+import axios from 'axios';
 // import useDailyEmailCheck from './hooks/useDailyEmailCheck';
 
 function App() {
+
+  // axios.defaults.baseURL = 'http://localhost:8000/api';
+  axios.defaults.baseURL = 'https://calendarling-backend.fly.dev/api';
+  axios.defaults.withCredentials = true; // Required for cookies to be sent
+
   return (
     <div className='App'>
       <BrowserRouter>
