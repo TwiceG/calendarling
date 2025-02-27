d328403a3c48783911c39edfc02a267dc729f206
TwiceG <g0stron0g@gmail.com>
2025-02-28 00:35:36 +0100

    rewrite rendering logic

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,26 +1,9 @@
 
 import { Link } from 'react-router-dom';
 import '../style/NavBar.css';
-import { useState, useEffect } from 'react';
-import axios from 'axios';
+
 
 const NavBar = () => {
-
-    const [isAuthenticated, setIsAuthenticated] = useState(false);
-
-    useEffect(() => {
-        axios.get('/me', { withCredentials: true })
-            .then(response => {
-                if (response.data && response.data.id) {  // Check if user exists
-                    setIsAuthenticated(true);
-                } else {
-                    setIsAuthenticated(false);
-                }
-            })
-            .catch(() => {
-                setIsAuthenticated(false);
-            });
-    }, []);
 
     return (
 
@@ -31,20 +14,22 @@ const NavBar = () => {
                 </li>
             </ul>
 
-            {isAuthenticated ? (
-                <div className="user-actions">
-                    <ul className="auth-list">
+            {localStorage.getItem('username') ? (
+                <div className='auth-list'>
+                    <div className="user-actions">
                         <li className="nav-item">
                             <Link to="/logout" className="nav-link">Logout</Link>
                         </li>
-                        <li className="nav-item">
-                            <Link to="/week-planner" className="nav-link">Planner</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link to="/chill" className="nav-link">Chill</Link>
-                        </li>
-                    </ul>
+                    </div>
+
+                    <li className="nav-item">
+                        <Link to="/week-planner" className="nav-link">Planner</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="/chill" className="nav-link">Chill</Link>
+                    </li>
                 </div>
+
             ) : (
                 <div className="user-actions">
                     <ul className="auth-list">
